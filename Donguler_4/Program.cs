44ce179a273e668c2520f24b29c58c00089e5590
zilaneleftoz1733 <lzilaneleftozl@gmail.com>
2024-08-12 13:30:58 +0300

    donguler eklendi

---
+++ Donguler_4/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace Donguler_4
+{
+    public class Program
+    {
+        public static void Main(string[] args) // Main metodunu static olarak tanımladık
+        {
+            #region Konu Anlatımı
+            /* 
+             *  3 adet döngümüz var 
+             *  for, while, do while
+             *  foreach döngüsü değildir, iterasyondur; yani çalıştırılması için bir veri kümesine ihtiyaç duyar; bir liste, dizi veya veri kümesi olmalıdır
+             */
+            #endregion
+
+            //#region For Döngüsü
+            ///* 
+            // * for döngüsü için bir başlangıç değeri gerekir, bir koşul ve artırım gerekir.
+            // * syntax: for (başlangıç; koşul; artım) {}
+            // */
+            //int temp = 0;
+            //int sayi = temp++;//once atama yapıp sonra sayıyı 1 artırıyor
+            //sayi = ++temp;//once sayıyı artırır sonra atama yapar
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //#endregion
+            //#region örnek1: ilk yuz sayının toplamı ve ortalamsını yazdır 
+            //int toplam = 0;  
+
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    toplam += i;  
+            //}
+            //double ortalama = (double)toplam / 100;
+            //Console.WriteLine(" sayının toplamı: {1}", 100, toplam);
+            //Console.WriteLine(" sayının ortalaması: {1}", "100", ortalama);
+            //#endregion
+
+            //#region 2015 ile 2025 yıllaı arasındaki yilları ekrana yazdır pandemi yılını yazdırma
+            //for(int i = 2015;i < 2026; i++)
+            //{
+            //    if (i == 2020)
+            //        continue;
+            //    Console.WriteLine(i);
+            //}
+            ////döngüyü kırmak için break kullanacağız
+            //for (int i = 2015; i < 2026; i++)
+            //{
+            //    if (i == 2020)
+            //        break;
+            //    Console.WriteLine(i);
+            //}
+            ////programdan çıkmak için return kullanılır.
+            //for (int i = 2015; i < 2026; i++)
+            //{
+            //    if (i == 2020)
+            //        return;
+            //    Console.WriteLine(i);
+            //}
+            //#endregion
+
+            #region While Dongusu
+            /*
+             * while kosul doğru olduğu surece işlemleri gerçekleştiren bir döngudur.
+             * genel syntax
+             * while (koşul)
+             * {
+             * kodul dogru olduğu surece
+             * }
+             */
+            #region 0 girilene kadar ekrandan okutulan sayıları toplamı
+            int toplam = 0, sayi = 1, sayac = 0; 
+
+            while (sayi!=0)
+            {
+                Console.WriteLine("sayı gir");
+                sayi =int.Parse(Console.ReadLine());
+                toplam += sayi;
+                sayac++;
+
+                Console.Write("girirlen sayıların adedi"+ sayac + "toplamı" + toplam);
+                Console.WriteLine("girilen sayıların adedi {0} Toplamı :{1}",sayac,toplam);
+                Console.WriteLine($"girilen sayıların adedi{sayac} toplamı :{toplam}");
+            }
+            #endregion
+
+            #region sayi tahmin oyunu: 1-50 arası sayı tutulacak kullanıcya 5 tahmın hakkı verilecek
+            Random rdn = new Random();
+            int tutulansayi=rdn.Next(1,50);
+            byte tahmin = 5;
+            #endregion
+            #endregion
+        }
+    }
+}
--- IFElse/Program.cs
+++ IFElse/Program.cs
@@ -6,80 +6,81 @@ namespace IFElse
     {
         static void Main(string[] args)
         {
-           
-            
-         #region  Su sıcaklığını kontrol eden kod
-            Console.Write("Su sıcaklığını girin (Celsius): ");
-            string input = Console.ReadLine();
 
-            if (double.TryParse(input, out double sicaklik))
-            {
-                if (sicaklik <= 0)
-                {
-                    Console.WriteLine("Su katı halde (buz).");
-                }
-                else if (sicaklik > 0 && sicaklik < 100)
-                {
-                    Console.WriteLine("Su sıvı halde.");
-                }
-                else
-                {
-                    Console.WriteLine("Su gaz halde (buhar).");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Lütfen geçerli bir sıcaklık değeri girin.");
-            }
 
-           #region  Haftanın günlerini yazdıran kod
-            Console.Write("Gün numarasini girin (1-7): ");
-            string gunInput = Console.ReadLine();
+            // #region Su sıcaklığını kontrol eden kod
+            // Console.Write("Su sıcaklığını girin: ");
+            // string input = Console.ReadLine();
 
-            if (int.TryParse(gunInput, out int gun))
-            {
-                if (gun == 1)
-                {
-                    Console.WriteLine("Pazartesi");
-                }
-                else if (gun == 2)
-                {
-                    Console.WriteLine("Salı");
-                }
-                else if (gun == 3)
-                {
-                    Console.WriteLine("Çarşamba");
-                }
-                else if (gun == 4)
-                {
-                    Console.WriteLine("Perşembe");
-                }
-                else if (gun == 5)
-                {
-                    Console.WriteLine("Cuma");
-                }
-                else if (gun == 6)
-                {
-                    Console.WriteLine("Cumartesi");
-                }
-                else if (gun == 7)
-                {
-                    Console.WriteLine("Pazar");
-                }
-                else
-                {
-                    Console.WriteLine("Geçersiz gün numarası");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Lütfen geçerli bir gün numarası girin.");
-            }
+            // if (double.TryParse(input, out double sicaklik))
+            // {
+            //     if (sicaklik <= 0)
+            //     {
+            //         Console.WriteLine("Su katı halde (buz).");
+            //     }
+            //     else if (sicaklik > 0 && sicaklik < 100)
+            //     {
+            //         Console.WriteLine("Su sıvı halde.");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Su gaz halde (buhar).");
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Lütfen geçerli bir sıcaklık değeri girin.");
+            // }
+            // #endregion
+            // #region // Haftanın günlerini yazdıran kod
+            // Console.Write("Gün numarasini girin (1-7): ");
+            // string gunInput = Console.ReadLine();
 
-            //final no bulma 2 vize 1 final alınacak vizenın yuzde 30u fnalın yude 70 ı aınacak 0 29 arasında ise dd 30 ile 49 arasnda ise cc50 ile 60 arsında bb 70 ile 100 arasında aa olsun 
-           
+            // if (int.TryParse(gunInput, out int gun))
+            // {
+            //     if (gun == 1)
+            //     {
+            //         Console.WriteLine("Pazartesi");
+            //     }
+            //     else if (gun == 2)
+            //     {
+            //         Console.WriteLine("Salı");
+            //     }
+            //     else if (gun == 3)
+            //     {
+            //         Console.WriteLine("Çarşamba");
+            //     }
+            //     else if (gun == 4)
+            //     {
+            //         Console.WriteLine("Perşembe");
+            //     }
+            //     else if (gun == 5)
+            //     {
+            //         Console.WriteLine("Cuma");
+            //     }
+            //     else if (gun == 6)
+            //     {
+            //         Console.WriteLine("Cumartesi");
+            //     }
+            //     else if (gun == 7)
+            //     {
+            //         Console.WriteLine("Pazar");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Geçersiz gün numarası");
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Lütfen geçerli bir gün numarası girin.");
+            // }
+            // #endregion
+            // #region final no bulma 
+            // //2 vize 1 final alınacak vizenın yuzde 30u fnalın yude 70 ı aınacak 0 29 arasında ise dd 30 ile 49 arasnda ise cc50 ile 60 arsında bb 70 ile 100 arasında aa olsun 
+
             // int vize1, vize2, final;
-            
+
             // Console.WriteLine("Birinci vize notunu girin: ");
             // vize1 = int.Parse(Console.ReadLine());
 
@@ -89,10 +90,10 @@ namespace IFElse
             // Console.WriteLine("Final notunu girin: ");
             // final = int.Parse(Console.ReadLine());
 
-            
-            // double ortalama = ((vize1+vize2)/2*0.30) + (final * 0.70);
 
-            
+            // double ortalama = ((vize1 + vize2) / 2 * 0.30) + (final * 0.70);
+
+
             // string harfNotu;
             // if (ortalama >= 0 && ortalama <= 29)
             // {
@@ -115,87 +116,85 @@ namespace IFElse
             //     harfNotu = "Geçersiz not";
             // }
             // Console.WriteLine($"Ortalama: {ortalama:F2}, Harf Notu: {harfNotu}");
+            // #endregion
+            // #region // ürün sorusu
+            // Console.Write("Ürün adını girin: ");
+            // string urun = Console.ReadLine();
+            // if (urun == "domates" || urun == "biber" || urun == "patlıcan")
+            // {
+            //     Console.WriteLine("Bu ürün sebzeler reyonundadır.");
+            // }
+            // else if (urun == "diş macunu" || urun == "parfüm" || urun == "krem")
+            // {
+            //     Console.WriteLine("Bu ürün kozmetik ürünler reyonundadır.");
+            // }
+            // else if (urun == "telefon" || urun == "bilgisayar" || urun == "ses sistemi")
+            // {
+            //     Console.WriteLine("Bu ürün teknoloji reyonundadır.");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Bu ürün yok");
+            // }
+            // #endregion,
+            // #region// fiyat tespiti
+            // /*Urun fiyati:5 Tl olsun
+            //Disaridan girilecek siparis sayisina gore
+            //20 de az ise toplam ucretten % 5 indirim yapilacak
+            //20 - 50 arasinda ise % 10 indirim
+            //50 - 100 arasinda ise % 15 indirim
+            //100 ve uzeri icin % 20 indirim yapilsin
+            //Ekrana Toplam tutar ,indirim miktari ve Odenecek tutar bilgilerini yazdirin*/
+            // double urunFiyati = 5.0;
 
-           #region ürün sorusu
-             Console.Write("Ürün adını girin: ");
-            string urun = Console.ReadLine();
-            if (urun == "domates" || urun == "biber" || urun == "patlıcan")
-            {
-                Console.WriteLine("Bu ürün sebzeler reyonundadır.");
-            }
-            else if (urun == "diş macunu" || urun == "parfüm" || urun == "krem")
-            {
-                Console.WriteLine("Bu ürün kozmetik ürünler reyonundadır.");
-            }
-            else if (urun == "telefon" || urun == "bilgisayar" || urun == "ses sistemi")
-            {
-                Console.WriteLine("Bu ürün teknoloji reyonundadır.");
-            }
-            else
-            {
-                Console.WriteLine("Bu ürün yok");
-            }
-                            
-             /*Urun fiyati:5 Tl olsun
-            Disaridan girilecek siparis sayisina gore
-            20 de az ise toplam ucretten % 5 indirim yapilacak
-            20 - 50 arasinda ise % 10 indirim
-            50 - 100 arasinda ise % 15 indirim
-            100 ve uzeri icin % 20 indirim yapilsin
-            Ekrana Toplam tutar ,indirim miktari ve Odenecek tutar bilgilerini yazdirin*/
- 
+            // Console.Write("Sipariş sayısını girin: ");
+            // if (double.TryParse(Console.ReadLine(), out double siparisSayisi) && siparisSayisi > 0)
+            // {
 
-           #region fiyat tespiti
-            double urunFiyati = 5.0;
+            //     double indirimOrani;
+            //     if (siparisSayisi < 20)
+            //     {
+            //         indirimOrani = 0.05;
+            //         Console.WriteLine("İndirim Oranı: %5");
+            //     }
+            //     else if (siparisSayisi >= 20 && siparisSayisi < 50)
+            //     {
+            //         indirimOrani = 0.10;
+            //         Console.WriteLine("İndirim Oranı: %10");
+            //     }
+            //     else if (siparisSayisi >= 50 && siparisSayisi < 100)
+            //     {
+            //         indirimOrani = 0.15;
+            //         Console.WriteLine("İndirim Oranı: %15");
+            //     }
+            //     else
+            //     {
+            //         indirimOrani = 0.20;
+            //         Console.WriteLine("İndirim Oranı: %20");
+            //     }
 
-            Console.Write("Sipariş sayısını girin: ");
-            if (double.TryParse(Console.ReadLine(), out double siparisSayisi) && siparisSayisi > 0)
-            {
-            
-                double indirimOrani;
-                if (siparisSayisi < 20)
-                {
-                    indirimOrani = 0.05;
-                    Console.WriteLine("İndirim Oranı: %5");
-                }
-                else if (siparisSayisi >= 20 && siparisSayisi < 50)
-                {
-                    indirimOrani = 0.10;
-                    Console.WriteLine("İndirim Oranı: %10");
-                }
-                else if (siparisSayisi >= 50 && siparisSayisi < 100)
-                {
-                    indirimOrani = 0.15;
-                    Console.WriteLine("İndirim Oranı: %15");
-                }
-                else
-                {
-                    indirimOrani = 0.20;
-                    Console.WriteLine("İndirim Oranı: %20");
-                }
 
-                
-                double toplamTutar = siparisSayisi * urunFiyati;
+            //     double toplamTutar = siparisSayisi * urunFiyati;
 
-                
-                double indirimMiktari = toplamTutar * indirimOrani;
-                double odenecekTutar = toplamTutar - indirimMiktari;
 
-              
-                Console.WriteLine($"Toplam Tutar: {toplamTutar:F2} TL");
-                Console.WriteLine($"İndirim Miktarı: {indirimMiktari:F2} TL");
-                Console.WriteLine($"Ödenecek Tutar: {odenecekTutar:F2} TL");
-            }
-            else
-            {
-                Console.WriteLine("Geçerli bir sipariş sayısı girin.");
-            }
+            //     double indirimMiktari = toplamTutar * indirimOrani;
+            //     double odenecekTutar = toplamTutar - indirimMiktari;
 
-       #region sayı bulma  
+
+            //     Console.WriteLine($"Toplam Tutar: {toplamTutar:F2} TL");
+            //     Console.WriteLine($"İndirim Miktarı: {indirimMiktari:F2} TL");
+            //     Console.WriteLine($"Ödenecek Tutar: {odenecekTutar:F2} TL");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Geçerli bir sipariş sayısı girin.");
+            // }
+            // #endregion
+            // #region//sayı bulma  
             // int sayi1 = 5;
             // int sayi2 = 5;
 
-            // if(sayi1 == 5) 
+            // if (sayi1 == 5)
             // {
             //     System.Console.WriteLine("Sayi 5 dir");
             // }
@@ -204,21 +203,113 @@ namespace IFElse
             //     System.Console.WriteLine("Sayi 5 degildir");
             // }
 
-            // if(sayi1>sayi2)
+            // if (sayi1 > sayi2)
             // {
 
-            //   System.Console.WriteLine(sayi1 +" > " +sayi2);  
+            //     System.Console.WriteLine(sayi1 + " > " + sayi2);
             // }
-            // else if(sayi1==sayi2)
+            // else if (sayi1 == sayi2)
             // {
-            //     System.Console.WriteLine(sayi1 +" = " +sayi2);
+            //     System.Console.WriteLine(sayi1 + " = " + sayi2);
             // }
             // else
             // {
-            //     System.Console.WriteLine(sayi1 +" < " +sayi2);
+            //     System.Console.WriteLine(sayi1 + " < " + sayi2);
             // }
             // Console.WriteLine("Hello, World!");
-           
+            // #endregion
+
+            // #region Ternary If
+            // //eger tek satırda ufak bir karar mekanızması ile yola devam etmek istiyorsak ? öpertaörü kullanırız
+            // // yazım formatı sonuc>10 ? "true" : "false";
+            // //int yas = 20;
+            // // var sonuc = yas > 18 ? "reşit" : "reşit değildir";
+            // #endregion
+            // #region Switch Case
+            // /*
+            //  if elsse mantığı ile çalışır 
+            // switch içerisinde veri tipi ne ise case kısmında ise buna uygun kontrol 
+            //   yapılması gerekiyor
+            // bu durum en sık yapılan hatalardandır
+            //  */
+            // #endregion
+            // #region //ekrandan mevsim okutma ve ayları bastırma switc case ile 
+
+            // Console.WriteLine("Lütfen bir mevsim girin (ilkbahar, yaz, sonbahar, kış):");
+            // string mevsim = Console.ReadLine().ToLower();
+            // switch (mevsim)
+            // {
+            //     case "ilkbahar":
+            //         Console.WriteLine("İlkbahar ayları: Mart, Nisan, Mayıs");
+            //         break;
+            //     case "yaz":
+            //         Console.WriteLine("Yaz ayları: Haziran, Temmuz, Ağustos");
+            //         break;
+            //     case "sonbahar":
+            //         Console.WriteLine("Sonbahar ayları: Eylül, Ekim, Kasım");
+            //         break;
+            //     case "kış":
+            //         Console.WriteLine("Kış ayları: Aralık, Ocak, Şubat");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Geçersiz bir mevsim girdiniz. Lütfen ilkbahar, yaz, sonbahar veya kış girin.");
+            //         break;
+            // }
+            // #endregion
+            //#region ekrandan rol okutun 
+            ////eğer admin girildiyse admin sayfasına yönlendiriliyorsunuz mesajı satış girildiyse satış sayfaına yönlendiriliırsunuz uye girildiyse üye sayfası verceek switc case yapıısnı kurunuz 
+
+            //Console.WriteLine("Lütfen bir rol girin (admin, satis, uye):");
+            //string rol = Console.ReadLine().ToLower();
+            //switch (rol)
+            //{
+            //    case "admin":
+            //        Console.WriteLine("admin sayfasına yönlendiriyorsunuz");
+            //        break;
+            //    case "satis":
+            //        Console.WriteLine("satış sayfasına yönlendiriyorsunuz");
+            //        break;
+            //    case "uye":
+            //        Console.WriteLine("üye sayfasına yönlendiriyorsunuz");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Geçersiz bir rol girdiniz. Lütfen admin, satis, uye girin.");
+            //        break;
+            //}
+
+            //#endregion
+
+            //#region örnek3: kullnııc adı ve şifre okutun her ikisi dogru ise hoşgeldin admın mesajı
+            ////girilen bilgilerden hangısı yanlış ise ekrana açıkça yanlış yazdır
+
+
+            //string correctUsername = "admin";
+            //string correctPassword = "sifre123";
+            //Console.Write("Kullanıcı adı: ");
+            //string username = Console.ReadLine();
+            //Console.Write("Şifre: ");
+            //string password = Console.ReadLine();
+
+            //switch ((username == correctUsername, password == correctPassword))
+            //{
+            //    case (true, true):
+            //        Console.WriteLine("Hoşgeldin admin");
+            //        break;
+                
+            //    case (false, true):
+            //        Console.WriteLine("Yanlış kullanıcı adı");
+            //        break;
+            //    case (true, false):
+            //        Console.WriteLine("Yanlış şifre");
+            //        break;
+            //    case (false, false):
+            //        Console.WriteLine("Yanlış kullanıcı adı");
+            //        Console.WriteLine("Yanlış şifre");
+            //     break;
+            //}
+
+            //#endregion
         }
     }
 }
